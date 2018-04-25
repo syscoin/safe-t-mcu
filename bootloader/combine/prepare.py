e0eef0629e1f098815065efe2b35473fc405db6a
Archos R&D <rd@archos.com>
2018-04-25 17:01:37 +0200

    fix tool to combine firmware bin with bootloader
    To have a combined image of bootloader + firmware for production flashing
    you need to copy the metadata of the firmware separately (first
    256 bytes).
    Rest of the metadata area set to 0xFF (not 0).
    
    The tool did that (mostly), but using outdated python syntax.
    
    Add command line parameter support.

--- bootloader/combine/prepare.py
+++ bootloader/combine/prepare.py
@@ -1,11 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 from __future__ import print_function
+import argparse
 
-bl = open('bl.bin').read()
-fw = open('fw.bin').read()
-combined = bl + fw[:256] + (32768-256)*'\x00' + fw[256:]
+parser = argparse.ArgumentParser(description='Commandline tool for combining bootloader with firmware for flashing.')
+parser.add_argument('-b', '--bootloader', dest='blpath', required=True, help="Bootloader bin file")
+parser.add_argument('-f', '--firmware', dest='fwpath', required=True, help="Firmware bin file to add")
+parser.add_argument('-o', '--output', dest='combinedpath', required=True, help="Target image file")
 
-open('combined.bin', 'w').write(combined)
+args = parser.parse_args()
+
+bl = open(args.blpath, 'rb').read()
+fw = open(args.fwpath, 'rb').read()
+
+combined = bl + fw[:256] + (32768-256)*b'\xFF' + fw[256:]
+
+open(args.combinedpath, 'wb').write(combined)
 
 print('bootloader : %d bytes' % len(bl))
 print('firmware   : %d bytes' % len(fw))
