7c23a876cd3abdeefc6d3a6c96079dfbcfa7dd3f
Archos R&D <rd@archos.com>
2018-06-13 09:48:19 +0200

    get_features reports number of remaining cryptomem zones
    We have 4 cryptomem zones only, report the remaining zones
    
    (needs update of protobuf messages!)

--- firmware/fsm.c
+++ firmware/fsm.c
@@ -315,7 +315,9 @@ void fsm_msgGetFeatures(GetFeatures *msg)
 	resp->has_needs_backup = true; resp->needs_backup = storage_needsBackup();
 	resp->has_flags = true; resp->flags = storage_getFlags();
 	resp->has_model = true; strlcpy(resp->model, "1", sizeof(resp->model));
-
+#if CRYPTOMEM
+	resp->has_pin_remaining_zones = true; resp->pin_remaining_zones = storage_remaining_zones();
+#endif
 	msg_write(MessageType_MessageType_Features, resp);
 }
 
